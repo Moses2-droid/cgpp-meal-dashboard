125df3d40dcd3c3d67efea74ca1a0b03bb242580
Moses2-droid <sindanimoise985@gmail.com>
2026-09-03 03:41:47 +0200

    Update App.py

--- App.py
+++ App.py
@@ -32,6 +32,7 @@ except Exception as e:
 colonnes_bool = df_raw.select_dtypes(include="bool").columns
 # Convertir True → 1 et False → 0
 df_raw[colonnes_bool] = df_raw[colonnes_bool].astype(int)
+st.dataframe(df_raw(colonnes_bool))
 # Optional lists: if empty, the app will try to detect variables automatically
 NOTIFICATION_VARIABLES = df_raw.iloc[:, 38:110].select_dtypes(include="int").columns.tolist()
 SENSITIZATION_VARIABLES = df_raw.iloc[:, [154] + list(range(156, 172)) + list(range(176, 192))+list(range(301, 305))].columns.tolist()
