1ab09347255fcd328b48e5cc8f09064d5a809a68
Moses2-droid <sindanimoise985@gmail.com>
2026-09-07 15:24:07 +0200

    Update App.py

--- App.py
+++ App.py
@@ -33,7 +33,7 @@ colonnes_bool = df_raw.select_dtypes(include="bool").columns.tolist()
 # Convertir True → 1 et False → 0
 df_raw[colonnes_bool] = df_raw[colonnes_bool].astype(int)
 # Optional lists: if empty, the app will try to detect variables automatically
-NOTIFICATION_VARIABLES = df_raw.iloc[:, 38:110].select_dtypes(include="int").columns.tolist()
+NOTIFICATION_VARIABLES = df_raw.iloc[:, 38:110].select_dtypes(include="number").columns.tolist()
 SENSITIZATION_VARIABLES = df_raw.iloc[:, [154] + list(range(156, 172)) + list(range(176, 192))+list(range(301, 305))].columns.tolist()
 VACCINATION_VARIABLES =df_raw.iloc[:, list(range(1196, 1221))+[1223]].columns.tolist()
 EBOLA_VARIABLES = df_raw.iloc[:, list(range(77, 79))+[159]+[82]].columns.tolist()
